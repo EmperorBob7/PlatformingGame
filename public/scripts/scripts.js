07e2265f149c9f3d6eb0f9f590ce15ce82f20f08
Glitch (bobplatformer) <none>
2021-10-09 22:49:06 +0000

    Transporting from Glitch

---
+++ public/scripts/scripts.js
@@ -0,0 +1,33 @@
+import Block from "./Block.js";
+import Character from "./Character.js";
+import items from "./round1/items.js";
+import {keys} from "./Keys.js";
+import {canvas, ctx} from "./ctx.js";
+
+const player = new Character(0, 400, 20, 20, ctx, items);
+
+let fps = Date.now() / 1000;
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  for (const [key, value] of Object.entries(items)) {
+    value.update();
+  }
+  requestAnimationFrame(draw);
+  fpsView();
+}
+
+function fpsView() {
+  let d = Date.now() / 1000;
+  let x = Date.now() / 1000 - fps;
+  fps = d;
+  document.getElementById("fps").innerText = (1 / x).toFixed(0);
+}
+
+requestAnimationFrame(draw);
+document.getElementById("updateCoords").onclick =  () => {
+  let x = Number(document.getElementById("xCoord").value);
+  let y = Number(document.getElementById("yCoord").value);
+  player.x = x;
+  player.y = y;
+}
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
